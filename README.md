ff52d4d6e85c8a00238a9e445f114294b9cb64b6
Floorbear <floorbear2723@gmail.com>
2024-02-02 15:20:34 +0900

    초기 커밋 메시지입니다.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectGrizzly
+
+Developed with Unreal Engine 5
---
+++ Source/ProjectGrizzly/Character/GrizzlyPC.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GrizzlyPC.generated.h"
+
+/**
+ * 
+ * 
+ */
+UCLASS()
+class PROJECTGRIZZLY_API AGrizzlyPC : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
