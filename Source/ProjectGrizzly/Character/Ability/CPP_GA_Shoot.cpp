5194834a5c78a2630e4f0b5b77cdf15b7a4a763e
Floorbear <floorbear2723@gmail.com>
2024-02-28 18:46:53 +0900

    이동원 : 절차적 반동 패턴 추가 완료

--- Source/ProjectGrizzly/Character/Ability/CPP_GA_Shoot.cpp
+++ Source/ProjectGrizzly/Character/Ability/CPP_GA_Shoot.cpp
@@ -7,11 +7,29 @@
 #include "AbilitySystemComponent.h"
 #include "Abilities\Tasks\AbilityTask_PlayMontageAndWait.h"
 #include "Abilities\Tasks\AbilityTask_WaitGameplayEvent.h"
+#include "Abilities\Tasks\AbilityTask_WaitDelay.h"
+#include "Kismet\KismetMathLibrary.h"
+#include "TickTask.h"
+#include <Curves\CurveVector.h>
 
 UCPP_GA_Shoot::UCPP_GA_Shoot()
 {
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	bReplicateInputDirectly = true;
 	InputID = EPGAbilityInputID::Shoot;
+}
+
+
+void UCPP_GA_Shoot::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
+	bPressed = false;
+}
+
+void UCPP_GA_Shoot::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	Super::InputPressed(Handle, ActorInfo, ActivationInfo);
+	bPressed = true;
 }
 
 void UCPP_GA_Shoot::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
@@ -21,28 +39,145 @@ void UCPP_GA_Shoot::ActivateAbility(const FGameplayAbilitySpecHandle Handle, con
 		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
 	}
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	RecoilTime = 0;
+	ShootCount = 0;
+	bPressed = true;
 
-	ACPP_PlayableCharacter* Player = Cast<ACPP_PlayableCharacter>(CurrentActorInfo->AvatarActor);
-	Player->GetHandsMeshComponent()->GetAnimInstance()->Montage_Play(AM_Hands_Shoot);
-	Player->GetFPWeaponMeshComponent()->GetAnimInstance()->Montage_Play(AM_Weapon_Shoot);
-	bool bADS = Player->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(TEXT("Ability.State.ADS")));
-	UAbilityTask_PlayMontageAndWait* Task = NULL;
-	if (bADS)
-	{
-		Task = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, AM_ADS_Shoot,1.0f,NAME_None,false);
+	UTickTask* TickTask = UTickTask::CreateTickTask(this, NAME_None);
+	TickTask->OnTick.AddDynamic(this,&UCPP_GA_Shoot::Tick);
+	TickTask->ReadyForActivation();
+	Shoot();
+}
+
+void UCPP_GA_Shoot::OnEndAbility(FGameplayEventData Payload)
+{
+	if (CanContinueShoot())
+	{		
+		Shoot();
 	}
 	else
 	{
-		Task = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, AM_Hip_Shoot, 1.0f, NAME_None, false);
+		//Recoil 회복이 끝나면 어빌리티를 종료하겠다
+		WaitTask = UAbilityTask_WaitDelay::WaitDelay(this, RecoilRecoveryTime);
+		WaitTask->OnFinish.AddDynamic(this, &UCPP_GA_Shoot::OnShootWaitFinish);
+		WaitTask->ReadyForActivation();
 	}
-	Task->ReadyForActivation();
+}
 
-	UAbilityTask_WaitGameplayEvent* EventReceivedTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, FGameplayTag::RequestGameplayTag(("Event.Montage.EndAbility")));
-	EventReceivedTask->EventReceived.AddDynamic(this, &UCPP_GA_Shoot::EventReceivedd);
+bool UCPP_GA_Shoot::CanContinueShoot()
+{
+	if (!bPressed)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void UCPP_GA_Shoot::Shoot()
+{
+	RecoilTime = 0;
+	ShootCount++;
+
+	ACPP_PlayableCharacter* Player = Cast<ACPP_PlayableCharacter>(CurrentActorInfo->AvatarActor);
+
+	//RPM 으로 몽타주 Rate 계산
+	// 기준 : 1.0 Rate = 400 RPM = 0.15초당 1발
+	// Ex ) 800 RPM = 0.075초당 1발 = 2.0Rate
+	float RPM = GetRPM();
+	float AnimationRate = RPM / 400.0f;
+
+	// 몽타주 재생
+	Player->GetHandsMeshComponent()->GetAnimInstance()->Montage_Play(AM_Hands_Shoot, AnimationRate);
+	Player->GetFPWeaponMeshComponent()->GetAnimInstance()->Montage_Play(AM_Weapon_Shoot, AnimationRate);
+	bool bADS = Player->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(TEXT("Ability.State.ADS")));
+	if (IsValid(MontageTask))
+	{
+		MontageTask->EndTask();
+	}
+	if (IsValid(EventReceivedTask))
+	{
+		EventReceivedTask->EndTask();
+	}
+	if (bADS)
+	{
+		MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, AM_ADS_Shoot, AnimationRate, NAME_None, false);
+	}
+	else
+	{
+		MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, AM_Hip_Shoot, AnimationRate, NAME_None, false);
+	}
+
+	//카메라 흔들림
+	CurrentActorInfo->PlayerController->PlayerCameraManager->StartCameraShake(VisualRecoil);
+	MontageTask->ReadyForActivation();
+
+
+
+	// 사격이 끝난 후 콜백 등록
+	EventReceivedTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, FGameplayTag::RequestGameplayTag(("Event.Montage.EndAbility")));
+	EventReceivedTask->EventReceived.AddDynamic(this, &UCPP_GA_Shoot::OnEndAbility);
 	EventReceivedTask->ReadyForActivation();
 }
 
-void UCPP_GA_Shoot::EventReceivedd(FGameplayEventData Payload)
+float UCPP_GA_Shoot::GetRPM()
+{
+	return 650.0f;
+}
+
+void UCPP_GA_Shoot::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	//현재 진행중인 테스크를 전부 종료
+	if (IsValid(MontageTask))
+	{
+		MontageTask->EndTask();
+	}
+	if (IsValid(EventReceivedTask))
+	{
+		EventReceivedTask->EndTask();
+	}
+	MontageTask = NULL;
+	EventReceivedTask = NULL;
+	WaitTask = NULL;
+
+	//멤버변수 초기화
+	RecoilTime = 0;
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UCPP_GA_Shoot::Tick(float _DeltaTime)
+{
+	//에임 회복 상태에서 사격하면 사격 어빌리티 비활성화 하는 명령을 중지하고 사격을 계속 진행
+	if (IsValid(WaitTask))
+	{
+		if (bPressed)
+		{
+			WaitTask->EndTask();
+			WaitTask = NULL;
+			Shoot();
+		}
+	}
+
+	float Rate = GetRPM() / 400.f;
+	RecoilTime = RecoilTime + _DeltaTime * Rate;
+
+	//반동
+	//반동 처리를 여기서 하는 이유 : 반동 그래프는 '연속적'이므로
+	float Pitch = WeaponRecoilCurve->GetVectorValue(RecoilTime).X;
+	float Yaw = WeaponRecoilCurve->GetVectorValue(static_cast<float>(ShootCount - 1) / static_cast<float>(MaxShootCount)).Y; // Yaw 그래프는 0~1 사이의 값, 따라서 mapping 필요
+	float PitchWeight = WeaponRecoilCurve->GetVectorValue(static_cast<float>(ShootCount - 1) / static_cast<float>(MaxShootCount)).Z;
+
+	if (Pitch > 0.f)
+	{
+		//PitchWeight = UKismetMathLibrary::RandomFloatInRange(1.0, PitchWeight);
+		Pitch *= PitchWeight;
+	}
+	Pitch *= -1.f;
+	CurrentActorInfo->PlayerController->AddPitchInput(Pitch);
+	CurrentActorInfo->PlayerController->AddYawInput(Yaw);
+}
+
+void UCPP_GA_Shoot::OnShootWaitFinish()
 {
 	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
 }
--- Source/ProjectGrizzly/Character/Ability/CPP_GA_Shoot.h
+++ Source/ProjectGrizzly/Character/Ability/CPP_GA_Shoot.h
@@ -17,6 +17,14 @@ class PROJECTGRIZZLY_API UCPP_GA_Shoot : public UGrizzlyAbility
 public:
 	UCPP_GA_Shoot();
 	
+	void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Input")
+	bool bPressed = true;
+
+
+	void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
 protected:
 	void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
@@ -30,5 +38,49 @@ protected:
 	UAnimMontage* AM_ADS_Shoot;
 
 	UFUNCTION()
-	void EventReceivedd(FGameplayEventData Payload);
+	void OnEndAbility(FGameplayEventData Payload);
+
+	UFUNCTION(BlueprintCallable)
+	bool CanContinueShoot();
+
+	UFUNCTION(BlueprintCallable)
+	void Shoot();
+
+	UFUNCTION(BlueprintCallable)
+	float GetRPM();
+
+
+	void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+private:
+	UFUNCTION()
+	void Tick(float _DeltaTime);
+
+	// ------------------------
+	// --------Recoil----------
+	// ------------------------
+private:
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Recoil", meta = (AllowPrivateAccess = "true"))
+	class UCurveVector* WeaponRecoilCurve;
+	class UAbilityTask_PlayMontageAndWait* MontageTask;
+	class UAbilityTask_WaitGameplayEvent* EventReceivedTask;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Recoil", meta = (AllowPrivateAccess = "true"))
+	class UAbilityTask_WaitDelay* WaitTask;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Recoil", meta = (AllowPrivateAccess = "true"))
+	float RecoilTime;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Recoil", meta = (AllowPrivateAccess = "true"))
+	int ShootCount = 0;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Recoil", meta = (AllowPrivateAccess = "true"))
+	int MaxShootCount = 30;
+
+	UFUNCTION()
+	void OnShootWaitFinish();
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Recoil", meta = (AllowPrivateAccess = "true"))
+	float RecoilRecoveryTime = 0.15f;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category = "Recoil", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class UCameraShakeBase> VisualRecoil;
+
 };
